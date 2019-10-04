c622fe9d86137b4e8f7bf6a8f194b34d325ec04d
Wouter ten Barge <wtbarge@gmail.com>
2019-10-04 17:01:59 +0200

    message

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //maak een array met namen
-
 var words = [ 
     "affenpinscher",
     "akita",
@@ -12,9 +11,12 @@ var words = [
     "leonberger",
     "Poedel",
 ];
-
 //kiezen van een random naam
 /*Math.floor(Math.random() * words.length);     // geeft een woord terug met de lengte van dat woord*/
-
 var word = words[Math.floor(Math.random() * words.length)];
-
+// Split random word from array into letters, but first the word to lowercase
+const splitWord = function(word) {
+    word = word.toLowerCase();
+    return word.split('');
+};
+let wordToBeGuessed = splitWord('word');
