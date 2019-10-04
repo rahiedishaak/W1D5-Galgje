fb19016e7cf0aaa49ba664cb3c4d1270dc99cc77
Wouter ten Barge <wtbarge@gmail.com>
2019-10-04 16:30:11 +0200

    message

--- script.js
+++ script.js
@@ -14,15 +14,7 @@ var words = [
 ];
 
 //kiezen van een random naam
-/*Math.floor(Math.random() * words.lenght);     // geeft een woord terug met de lengte van dat woord*/
+/*Math.floor(Math.random() * words.length);     // geeft een woord terug met de lengte van dat woord*/
 
-var words = [words[Math.floor(Math.random() * words.lenght)]];
+var word = words[Math.floor(Math.random() * words.length)];
 
-/*het antwoord = []; 
-dus als i=0 en i= kleiner dan de word lengte dan i+1 */
-
-for (var i = 0; i < word.length; i++) {
-    answerArray[i] = "_";
-  }
-  
-  var remainingLetters = word.length;
