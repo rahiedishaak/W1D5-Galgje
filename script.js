345ac70d028344cdaf29209f6ce7375effcb4c8b
Rahied Ishaak <rahied.ishaak.code@gmail.com>
2019-10-04 17:56:45 +0200

    adds blanks in DOM for each letter

--- script.js
+++ script.js
@@ -11,12 +11,24 @@ var words = [
     "leonberger",
     "Poedel",
 ];
+
 //kiezen van een random naam
 /*Math.floor(Math.random() * words.length);     // geeft een woord terug met de lengte van dat woord*/
 var word = words[Math.floor(Math.random() * words.length)];
+
 // Split random word from array into letters, but first the word to lowercase
 const splitWord = function(word) {
     word = word.toLowerCase();
     return word.split('');
 };
-let wordToBeGuessed = splitWord('word');
+
+let wordToBeGuessed = splitWord(word);
+
+// render underscore in DOM for each letter
+const lettersElement = document.querySelector('#letters');
+
+wordToBeGuessed.forEach(letter => {
+    const span = document.createElement('span');
+    span.textContent = '_';
+    lettersElement.appendChild(span);    
+});
